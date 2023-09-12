b3847c710bcf06b825043a93f12a3b462deaf272
archeryfox <89808784+archeryfox@users.noreply.github.com>
2023-09-12 22:41:38 +0300

    тдд

---
+++ Strings.Tests/StringsTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Strings.Tests
+{
+    [TestClass]
+    public class StringsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
